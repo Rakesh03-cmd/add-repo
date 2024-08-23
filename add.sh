7e822bc6f43eeede3660d078f06f282543000b3c
Rakesh03-cmd <rakeshtummla@gmail.com>
2024-08-23 14:45:29 +0530

    msg1

---
+++ add.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:22 "
+
+
+# Ask user for second number
+echo "Enter second number:97 "
+
+
+# Perform addition
+sum=$((22 + 97))
+
+# Display result
+echo "Sum: $sum"
